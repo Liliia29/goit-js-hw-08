5cfc2b39c40d950b762074c31eb2848e13791ac6
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-04-11 21:26:26 +0300

    js-1-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = createImageCardMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+imageContainer.addEventListener('click', onImageContainerClick);
+function createImageCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>`
+    }).join('');
+}
+function onImageContainerClick(evt) {
+    const isImgSwatchEl = evt.target.classList.contains('gallery__image');
+    if (!isImgSwatchEl) {
+        return;
+    }
+    evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      lightbox.open();
+}
+ 
 console.log(galleryItems);
+
